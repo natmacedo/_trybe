76311809400b69d6dc50c1e76147078529435da2
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-05-29 14:31:56 -0300

    Estrutura final com campos e fieldset do form

---
+++ 6_1/script.js
@@ -0,0 +1,15 @@
+
+let estados = [ 'Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo',
+'Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco',
+'Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+let list=document.getElementById("lista-estados");
+
+
+/*for (list of estados) {
+  let option = document.create("option");
+  let element = list.appendChild(option);
+  element.innerText = item[0];
+  element.value = item[1];
+}*/
+
+function preventDefault() 

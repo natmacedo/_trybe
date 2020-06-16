709507fd063ae29ef8bfbaae14df715ecefa8153
Natalia Macedo <nataliasilvamacedo@gmail.com>
2020-06-16 18:55:11 -0300

    Exercicio 1

--- 8_1/exercises1.js
+++ 8_1/exercises1.js
@@ -1,70 +1,76 @@
-const assert = require('assert');
+const assert = require("assert");
+const { verify } = require("crypto");
 
 const books = [
   {
     id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
     author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948
+      name: "George R. R. Martin",
+      birthYear: 1948,
     },
     releaseYear: 1991,
   },
   {
     id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
     author: {
-      name: 'J. R. R. Tolkien',
+      name: "J. R. R. Tolkien",
       birthYear: 1892,
     },
     releaseYear: 1954,
   },
   {
     id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
+    name: "Fundação",
+    genre: "Ficção Científica",
     author: {
-      name: 'Isaac Asimov',
+      name: "Isaac Asimov",
       birthYear: 1920,
     },
     releaseYear: 1951,
   },
   {
     id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
+    name: "Duna",
+    genre: "Ficção Científica",
     author: {
-      name: 'Frank Herbert',
+      name: "Frank Herbert",
       birthYear: 1920,
     },
     releaseYear: 1965,
   },
   {
     id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
+    name: "A Coisa",
+    genre: "Terror",
     author: {
-      name: 'Stephen King',
-      birthYear: 1947
+      name: "Stephen King",
+      birthYear: 1947,
     },
     releaseYear: 1986,
   },
   {
     id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
     author: {
-      name: 'H. P. Lovecraft',
+      name: "H. P. Lovecraft",
       birthYear: 1890,
     },
     releaseYear: 1928,
   },
 ];
 
+// Encontre o nome da pessoa autora do livro nascida no ano de 1947
 function authorBornIn1947() {
-  // escreva aqui o seu código
+  const verifyName = (books) => (books.author.birthYear === 1947);
+  const authorName = books.find(verifyName);
+  return authorName.author.name;
 }
 
-assert.equal(authorBornIn1947(), 'Stephen King');
+console.log(authorBornIn1947());
+
+assert.equal(authorBornIn1947(), "Stephen King");
